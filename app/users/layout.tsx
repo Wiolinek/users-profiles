eb696ea50121b56bc09e1d8bbc345326eb010bdf
Wiola P <wiol@onet.eu>
2024-12-16 02:23:24 +0100

    add users layout component

---
+++ app/users/layout.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/Header";
+
+export default function UsersLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
